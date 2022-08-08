94d9c00a50d335dac41dfad17576073fb87bdee2
Handika Rafli A <shandika231@gmail.com>
2022-08-09 06:04:25 +0700

    solve repeat string

---
+++ dart_codewars_8kyu/bin/repeat_string.dart
@@ -0,0 +1,16 @@
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+void main() {
+  String repeatString(int n, String s) {
+    // your code here
+    String result = '';
+    for (var i = 0; i < n; i++) {
+      result += s;
+    }
+    return result;
+  }
+
+  var coba = repeatString(6, 'hello');
+  print(coba);
+}
