d28399df176e4a283b974955caad1ed55b6a96b9
Handika Rafli A <shandika231@gmail.com>
2022-08-13 07:41:55 +0700

    solve otherAngle

---
+++ dart_codewars_8kyu/bin/third_angle_of_triangle.dart
@@ -0,0 +1,10 @@
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+void main(List<String> args) {
+  int otherAngle(int a, int b) {
+    // your code here
+    return 180 - (a + b);
+  }
+}
