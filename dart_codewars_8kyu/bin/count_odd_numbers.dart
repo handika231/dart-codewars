3e28bf82e940e6a60039ac04a18251be2abebb19
Handika Rafli A <shandika231@gmail.com>
2022-08-13 07:45:15 +0700

    solve oddCount

---
+++ dart_codewars_8kyu/bin/count_odd_numbers.dart
@@ -0,0 +1,14 @@
+//Given a number n, return the number of positive odd numbers below n, EASY!
+
+main() {
+  int oddCount(n) {
+    // your code here
+    int result = 0;
+    for (int i = 1; i < n; i++) {
+      if (i % 2 == 1) {
+        result++;
+      }
+    }
+    return result;
+  }
+}
