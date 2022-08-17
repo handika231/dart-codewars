b907cd5e080dab6cdf908c86871f8996f4082e91
Handika Rafli A <shandika231@gmail.com>
2022-08-17 08:53:04 +0700

    messi goal function

---
+++ dart_codewars_8kyu/bin/messi_goal_function.dart
@@ -0,0 +1,5 @@
+// 5, 10, 2  -->  17
+main(List<String> args) {
+  int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) =>
+      laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
